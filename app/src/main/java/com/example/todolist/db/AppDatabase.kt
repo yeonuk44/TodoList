cb03624c85302b59cc6fa4090e33bd303dd96a5a
yeonuk44 <yeonuk44@gmail.com>
2023-07-25 17:14:26 +0900

    # Update AppDatabase file

--- app/src/main/java/com/example/todolist/db/AppDatabase.kt
+++ app/src/main/java/com/example/todolist/db/AppDatabase.kt
@@ -1,8 +1,32 @@
 package com.example.todolist.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-// Room library
+// Room library 사용 조건
+
+// 1. Entity 사용을 위한 바인딩
 @Database(entities = arrayOf(TodoEntity::class), version = 1)
-class AppDatabase {
+
+// 2. DB 사용을 위한 바인딩 및 추상화 작업
+abstract class AppDatabase : RoomDatabase(){
+
+    // 조회라는 행위를 사용하기 위한 TodoDao  바인딩
+    abstract fun getTodoDao() : TodoDao
+
+    companion object{
+        val databaseName = "db_todo"
+        var appDatabase : AppDatabase ?= null
+
+        fun getInstance(context: Context) : AppDatabase? {
+            if(appDatabase == null){
+                appDatabase = Room.databaseBuilder(context,
+                AppDatabase::class.java,
+                databaseName).fallbackToDestructiveMigration().build()
+            }
+            return appDatabase
+        }
+    }
 }
