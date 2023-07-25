3cf749be0d64588ed8278b47c4e4bb56928c0d54
yeonuk44 <yeonuk44@gmail.com>
2023-07-25 20:48:33 +0900

    # WIP: Update TodoRecyclerViewAdapter file

--- app/src/main/java/com/example/todolist/TodoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoRecyclerViewAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.todolist
 
-class TodoRecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ItemTodoBinding
+import com.example.todolist.db.TodoEntity
+
+class TodoRecyclerViewAdapter(private val todoList: ArrayList<TodoEntity>) : RecyclerView.Adapter<TodoRecyclerViewAdapter.MyViewHolder>() {
+    inner class MyViewHolder(binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root){
+        val tv_importance = binding.tvImportance
+        val tv_title = binding.tvTitle
+
+        val root = binding.root
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding : ItemTodoBinding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+    }
 }
