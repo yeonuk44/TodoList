a9667ea6c060bbce6eb8fa2603b185b127ca64e6
yeonuk44 <yeonuk44@gmail.com>
2023-07-25 18:54:01 +0900

    # Update binding code

--- app/src/main/java/com/example/todolist/AddTodoActivity.kt
+++ app/src/main/java/com/example/todolist/AddTodoActivity.kt
@@ -2,10 +2,26 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.todolist.databinding.ActivityMainBinding
 
 class AddTodoActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_todo)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+
+        setContentView(binding.root)
+
+        btn = findViewById(R.id.btn_add_todo)
+
+//        btn.setOnClickListener()
+//
+//        binding.btnAddTodo.setOnClickListener()
     }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.TodoDao
+import com.example.todolist.db.TodoEntity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var db : AppDatabase
+    private lateinit var todoDao : TodoDao
+    private lateinit var todoList: ArrayList<TodoEntity>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        getAllTodoList()
+    }
+
+    private fun getAllTodoList(){
+        Thread{
+            todoList = ArrayList(todoDao.getAllTodo())
+            setRecyclerView()
+
+        }
+    }
+
+    private fun setRecyclerView() {
+        TODO("Not yet implemented")
     }
 }
