cdc6afe06f21d19697b902bb70eedd6e6cc62341
yeonuk44 <yeonuk44@gmail.com>
2023-07-26 13:32:35 +0900

    # Update TodoRecyclerViewAdapter file

--- app/src/main/java/com/example/todolist/TodoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoRecyclerViewAdapter.kt
@@ -14,16 +14,41 @@ class TodoRecyclerViewAdapter(private val todoList: ArrayList<TodoEntity>) : Rec
         val root = binding.root
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
+    /**
+     * Info
+     * onCreateViewHolder: MyViewHolder의 데이터를 받아 ViewHoler 객체를 만든다.
+     * onBindViewHolder: MyViewHolder 객체에 데이터를 넣는다.
+     * getItemCount: 아이템의 수가 몇 개인지 반환하는 객체이다.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val binding : ItemTodoBinding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
+        return MyViewHolder(binding)
     }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val todoData = todoList[position]
+
+        when(todoData.importance){
+            1 -> {
+                holder.tv_importance.setBackgroundResource(R.color.red)
+            }
+            2 -> {
+                holder.tv_importance.setBackgroundResource(R.color.yellow)
+            }
+            3->{
+                holder.tv_importance.setBackgroundResource(R.color.green)
+
+            }
+        }
+
+        holder.tv_importance.text = todoData.importance.toString()
+        holder.tv_title.text = todoData.title
+    }
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+
+
+
+
 }
